b09fa8dcc88b4ad921fa17583188e5f90547b864
Matt Rayner <me@mattihew.com>
2017-06-16 20:37:54 +0100

    added ServerEndpointConfig class

---
+++ src/main/java/com/logdyn/api/endpoints/LoggingEndpointConfig.java
@@ -0,0 +1,83 @@
+package com.logdyn.api.endpoints;
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.Decoder;
+import javax.websocket.Encoder;
+import javax.websocket.Extension;
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Matt on 16/06/2017.
+ */
+public class LoggingEndpointConfig implements ServerEndpointConfig
+{
+    private final String path;
+    private final Map<String, Object> userParams = new HashMap<>();
+    public LoggingEndpointConfig(final String path)
+    {
+        this.path = path;
+    }
+
+    @Override
+    public Class<?> getEndpointClass()
+    {
+        return LoggingEndpoint.class;
+    }
+
+    @Override
+    public String getPath()
+    {
+        return this.path;
+    }
+
+    @Override
+    public List<String> getSubprotocols()
+    {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<Extension> getExtensions()
+    {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Configurator getConfigurator()
+    {
+        return new ServerEndpointConfig.Configurator()
+        {
+            @Override
+            public void modifyHandshake(final ServerEndpointConfig sec,
+                                        final HandshakeRequest request, final HandshakeResponse response)
+            {
+                sec.getUserProperties().put(HttpSession.class.getName(), request.getHttpSession());
+                super.modifyHandshake(sec, request, response);
+            }
+        };
+    }
+
+    @Override
+    public List<Class<? extends Encoder>> getEncoders()
+    {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<Class<? extends Decoder>> getDecoders()
+    {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Map<String, Object> getUserProperties()
+    {
+        return this.userParams;
+    }
+}
